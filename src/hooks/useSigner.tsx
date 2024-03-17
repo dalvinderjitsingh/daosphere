ba539493fa231b08a43fde0d7d19d3dd7f75ad98
Dalvinderjit Singh <66400608+dalvinderjitsingh@users.noreply.github.com>
2024-03-17 23:24:29 +0800

    Create useSigner.tsx

---
+++ src/hooks/useSigner.tsx
@@ -0,0 +1,29 @@
+// Convert wagmi/viem `Client` to ethers `Signer`
+import { useMemo } from "react";
+import { providers, type Signer } from "ethers";
+import { useWalletClient } from "wagmi";
+import type { Account, Chain, Client, Transport } from "viem";
+
+function walletClientToSigner(
+  walletClient: Client<Transport, Chain, Account>,
+): Signer {
+  const { account, chain, transport } = walletClient;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address,
+  };
+  const provider = new providers.JsonRpcProvider(transport.url, network);
+  const signer = provider.getSigner(account.address);
+  return signer;
+}
+
+export function useSigner({ chainId }: { chainId?: number } = {}):
+  | Signer
+  | undefined {
+  const { data: walletClient } = useWalletClient({ chainId });
+  return useMemo(
+    () => (walletClient ? walletClientToSigner(walletClient) : undefined),
+    [walletClient],
+  );
+}
