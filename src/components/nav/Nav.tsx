d209671e45e77ffbdd7d7fdc976cddc4bf6797ef
Dalvinderjit Singh <66400608+dalvinderjitsingh@users.noreply.github.com>
2024-03-17 23:23:58 +0800

    Update Nav.tsx

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,11 +2,16 @@
 
 import React, { useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
 import { ConnectKitButton, useModal } from "connectkit";
 import { useAccount, useDisconnect } from "wagmi";
+import { getUserByWalletAddress } from "@/lib/tablelandUtils";
 
 export default function Nav() {
+  const router = useRouter();
+
   const { openProfile, open } = useModal();
   const { isConnected, address, isConnecting } = useAccount();
 
@@ -17,6 +22,30 @@ export default function Nav() {
       // if yes, route to home/dashboard
     }
   }, [isConnected]);
+
+  // instead of isCOnnected i would like to use On COnnect, i dunno but feels as thou would be faster
+
+  useEffect(() => {
+    const checkUser = async () => {
+      if (isConnected) {
+        if (address !== undefined) {
+          let user = await getUserByWalletAddress(address);
+          if (user && user.wallet_address == address) {
+            // User exists, handle accordingly
+            router.push("/dashboard");
+          } else {
+            // User does not exist, handle accordingly
+            router.push("/signup");
+            // console.log(user);
+            // console.log(address);
+            // console.log(user && user.wallet_address);
+          }
+        }
+      }
+    };
+
+    checkUser();
+  }, [isConnected, address, router]);
 
   return (
     <nav className="fixed left-0 right-0 top-0 z-50">
