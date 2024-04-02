25594d431e44e7703052813b0756627faabc89ea
Dalvinderjit Singh <66400608+dalvinderjitsingh@users.noreply.github.com>
2024-04-02 23:31:05 +0800

    Update route.ts

--- src/app/api/users/query-users-table/route.ts
+++ src/app/api/users/query-users-table/route.ts
@@ -2,9 +2,17 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const users = await sql`SELECT * FROM Users;`;
-  return NextResponse.json({ users }, { status: 200 });
-  //return NextResponse.json({ pets: pets.rows }, { status: 200 });
+  try {
+    const users = await sql`SELECT * FROM Users;`;
+    return NextResponse.json({ users }, { status: 200 });
+    //return NextResponse.json({ pets: pets.rows }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return NextResponse.json(
+      { error: "An error occurred while fetching users." },
+      { status: 500 },
+    );
+  }
 }
 
-// http://localhost:3000/api/query-pets-table
+// http://localhost:3000/api/query-users-table
